41c2852491cb4196b24f1877546286b3277b1f97
Certurix <46199479+Certurix@users.noreply.github.com>
2024-10-07 20:42:38 +0200

    Couverture des tests pour __main__.py à 100%

--- tests/test_main.py
+++ tests/test_main.py
@@ -1,6 +1,20 @@
 from olympics.__main__ import main
-
+import pytest
+import argparse
 
 def test_countries():
-    argv = ['countries']
+    argv = ['countries', '--top', '10']
+    main(argv)
+
+def test_collective():
+    argv = ['collective', '--top', '5']
+    main(argv)
+
+def test_individual():
+    argv = ['individual', '--top', '6']
+    main(argv)
+
+@pytest.mark.xfail(raises=argparse.ArgumentTypeError)
+def test_args_top():
+    argv = ['countries', '--top', '-1']
     main(argv)
